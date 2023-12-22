36da1cc8116f1837101144b2f9e6114bd9ceef0d
Asus <Asus@DESKTOP-86T0M6A>
2023-12-22 17:59:10 +0200

    Add project files.

---
+++ CompanyDataCollector/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace CompanyDataCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new ChromeDriver(new ChromeOptions
+            {
+                BinaryLocation = "C:\\Users\\Asus\\source\\repos\\chromeDriver\\chrome.exe"
+            });
+            driver.Navigate().GoToUrl("https://www.kolzchut.org.il/he/קטגוריה:פורטלים");
+            //li[data-raofz="18"]
+        }
+    }
+}
